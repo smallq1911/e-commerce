eea28bbd44b442d0476a0ddcec7be9a06f63fae6
Le Thanh Quang <miraculousmut@gmail.com>
2023-08-24 15:05:16 +0700

    add side navigation for mobile and css

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -1,13 +1,13 @@
-function dropdownClick() {
-    document.getElementById("notiDropdown").classList.toggle("show");
+function showToggle() {
+    document.getElementById("toggle").classList.toggle("show")
   }
   
   // Close the dropdown menu if the user clicks outside of it
   window.onclick = function(event) {
-    if (!event.target.matches('.noti')) {
-      var dropdown = document.getElementById("notiDropdown");
-        if (dropdown.classList.contains('show')) {
-          dropdown.classList.remove('show');
+    if (!event.target.matches('.toggle-btn')) {
+      var element = document.getElementById("toggle")
+        if (element.classList.contains("show")) {
+          element.classList.remove("show")
         }
       }
     }
