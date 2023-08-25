e35661ea1accb2814b6a779bbf548e5af9b92891
Le Thanh Quang <miraculousmut@gmail.com>
2023-08-25 16:30:19 +0700

    add dropdown notification, favorite modal, cart overlay, search form, pc nav

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -1,14 +1,50 @@
-function showToggle() {
+// Initialize Swiper
+var swiper = new Swiper(".header__msg-block", {
+  autoHeight: true,
+  direction: "vertical",
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: true,
+  },
+});
+
+// Toggle show dropdown
+function ShowToggle() {
     document.getElementById("toggle").classList.toggle("show")
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.toggle-btn')) {
-      var element = document.getElementById("toggle")
-        if (element.classList.contains("show")) {
-          element.classList.remove("show")
-        }
-      }
+}
+
+function ShowFavorite() {
+  document.getElementById("modal").classList.add("show")
+}
+
+function ShowSearch() {
+  document.getElementById("searchForm").classList.toggle("search-show")
+}
+function ShowCart() {
+  document.getElementById("cart").classList.toggle("cart-show")
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.notification-btn')) {
+    let dropdown = document.getElementById("toggle");
+    if (dropdown.classList.contains('show')) {
+      dropdown.classList.remove('show');
     }
-  
+  }
+  if (!event.target.matches('.search-btn')) {
+    let dropdown = document.getElementById("searchForm");
+    if (dropdown.classList.contains('search-show')) {
+      dropdown.classList.remove('search-show');
+    }
+  }
+}
+
+var favoriteModal = document.getElementById("modal")
+favoriteModal.onclick = function() {
+  favoriteModal.classList.remove("show")
+}
+
+var cartSidebar = document.getElementById("cart")
+cartSidebar.onclick = function() {
+  cartSidebar.classList.remove("cart-show")
+}
